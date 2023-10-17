1586ccf4f652645f4da171ce2e53f585e146cfd3
aguimorefran <aguimorefran@gmail.com>
2023-10-17 17:40:59 +0200

    + Working api call to solve

--- balancer/api.py
+++ balancer/api.py
@@ -2,6 +2,7 @@ from typing import List
 from fastapi import FastAPI
 from db import DB_COLUMNS, execute_query
 from models import Food
+from balancer import solve_problem as solve
 
 app = FastAPI()
 
@@ -42,3 +43,92 @@ def search_food(name: str):
     fetched_foods = execute_query(f"SELECT * FROM food WHERE {where_clause}")
     foods = [map_row_to_food(food) for food in fetched_foods]
     return foods
+
+
+foods_test = [
+    {
+        "id": 103,
+        "search_term": "cacao puro valor",
+        "name": "Cacao Puro Natural 100%",
+        "category": "chocolate",
+        "subcategory": "cacao",
+        "brand": "Valor",
+        "item_url": "https://fatsecret.es/calor%C3%ADas-nutrici%C3%B3n/valor/cacao-puro-natural-100/100g",
+        "cals_per_g": 3.39,
+        "fat_per_g": 0.11,
+        "carb_per_g": 0.17,
+        "prot_per_g": 0.25,
+        "serving_size": 10,
+        "max_servings": 3,
+    },
+    {
+        "id": 101,
+        "search_term": "copos de avena",
+        "name": "Copos de Avena",
+        "category": "cereales",
+        "subcategory": "avena",
+        "brand": "Mercadona",
+        "item_url": "https://fatsecret.es/calor%C3%ADas-nutrici%C3%B3n/mercadona/copos-de-avena/100g",
+        "cals_per_g": 3.75,
+        "fat_per_g": 0.07,
+        "carb_per_g": 0.59,
+        "prot_per_g": 0.14,
+        "serving_size": 5,
+        "max_servings": 50,
+    },
+    {
+        "id": 39,
+        "search_term": "cacahuetes",
+        "name": "Cacahuetes Tostados",
+        "category": "frutos secos",
+        "subcategory": "cacahuetes",
+        "brand": "Carrefour",
+        "item_url": "https://fatsecret.es/calor%C3%ADas-nutrici%C3%B3n/carrefour/cacahuetes-tostados/100g",
+        "cals_per_g": 6.15,
+        "fat_per_g": 0.5,
+        "carb_per_g": 0.062,
+        "prot_per_g": 0.29,
+        "serving_size": 5,
+        "max_servings": 20,
+    },
+    {
+        "id": 67,
+        "search_term": "avellanas laminadas",
+        "name": "Avellanas Tostadas",
+        "category": "frutos secos",
+        "subcategory": "avellanas",
+        "brand": "Carrefour",
+        "item_url": "https://fatsecret.es/calor%C3%ADas-nutrici%C3%B3n/carrefour/avellanas-tostadas/100g",
+        "cals_per_g": 7.12,
+        "fat_per_g": 0.68,
+        "carb_per_g": 0.038,
+        "prot_per_g": 0.17,
+        "serving_size": 5,
+        "max_servings": 20,
+    },
+]
+target_kcals = 2000
+max_fat_pct = 0.25
+min_prot_pct = 0.40
+penalty_protein = 100
+penalty_fat = 100
+penalty_kcals = 10
+
+
+@app.get("/solve_problem")
+def solve_problem():
+    """
+    Calls the solve_problem function to solve a problem.
+    """
+    result = solve(
+        {
+            "target_kcals": target_kcals,
+            "max_fat_pct": max_fat_pct,
+            "min_prot_pct": min_prot_pct,
+            "foods": foods_test,
+            "penalty_protein": penalty_protein,
+            "penalty_fat": penalty_fat,
+            "penalty_kcals": penalty_kcals,
+        }
+    )
+    return {"result": result}
--- balancer/balancer.py
+++ balancer/balancer.py
@@ -10,130 +10,69 @@ MIN_GRAMS = 5
 MAX_GRAMS = 200
 
 
-def calculate_results(
-    prob,
-    food_names,
-    food_prots,
-    food_fats,
-    food_carbs,
-    food_cals,
-):
-    ret = []
-    # Print the name of the vars and the value
+def parse_result(prob):
+    result = {}
     for v in prob.variables():
         if v.varValue > 0:
-            food = {}
-            print(v.name, "=", v.varValue)
-            name_no_prefix = v.name.replace("food_", "")
-            if "multiplier" in v.name or "slack" in v.name:
-                continue
-            food["name"] = v.name
-            food["grams"] = v.varValue
-            food["protein_per_100g"] = round(
-                food_prots[food_names.index(name_no_prefix)] * 100, 3
-            )
-            food["fat_per_100g"] = round(
-                food_fats[food_names.index(name_no_prefix)] * 100, 3
-            )
-            food["carbs_per_100g"] = round(
-                food_carbs[food_names.index(name_no_prefix)] * 100, 3
-            )
-            food["kcals_per_100g"] = round(
-                food_cals[food_names.index(name_no_prefix)] * 100, 3
-            )
-
-            food["protein"] = round(food["protein_per_100g"] * food["grams"] / 100, 3)
-            food["fat"] = round(food["fat_per_100g"] * food["grams"] / 100, 3)
-            food["carbs"] = round(food["carbs_per_100g"] * food["grams"] / 100, 3)
-            food["kcals"] = round(food["kcals_per_100g"] * food["grams"] / 100, 3)
-            ret.append(food)
-
-    # Add the computation of the percentages of each macro by its kcals
-    fat_kcals = sum([f["fat"] for f in ret]) * KCALS_GRAM_FAT
-    carbs_kcals = sum([f["carbs"] for f in ret]) * KCALS_GRAM_CARBS
-    protein_kcals = sum([f["protein"] for f in ret]) * KCALS_GRAM_PROTEIN
-
-    total_kcals_by_macro = fat_kcals + carbs_kcals + protein_kcals
-    total_kcals = sum([f["kcals"] for f in ret])
-
-    ret.append(
-        {
-            "results": {
-                "fat_kcals": round(fat_kcals, 2),
-                "carbs_kcals": round(carbs_kcals, 2),
-                "protein_kcals": round(protein_kcals, 2),
-                "total_kcals": round(total_kcals, 2),
-                "total_kcals_by_macro": round(total_kcals_by_macro, 2),
-                "fat_pct": round(fat_kcals / total_kcals_by_macro, 2),
-                "carbs_pct": round(carbs_kcals / total_kcals_by_macro, 2),
-                "protein_pct": round(protein_kcals / total_kcals_by_macro, 2),
-            },
-        }
-    )
-
-    return ret
+            result[v.name] = v.varValue
+    return result
 
 
 def solve_problem(params):
-    target_kcals = params.get("target_kcals")
-    max_fat_pct = params.get("max_fat_pct", 0.25)
-    min_prot_pct = params.get("min_prot_pct", 0.40)
-    food = params.get("food")
-    penalty_protein = params.get("penalty_protein", 100)
-    penalty_fat = params.get("penalty_fat", 100)
-    penalty_kcals = params.get("penalty_kcals", 10)
+    target_kcals = params["target_kcals"]
+    max_fat_pct = params["max_fat_pct"]
+    min_prot_pct = params["min_prot_pct"]
+    foods = params["foods"]
+    penalty_protein = params["penalty_protein"]
+    penalty_fat = params["penalty_fat"]
+    penalty_kcals = params["penalty_kcals"]
 
-    food_names = [f["name"] for f in food]
-    food_names = [f.replace(" ", "_") for f in food_names]
-    food_cals = [f["cals_per_g"] for f in food]
-    food_fats = [f["fat_per_g"] for f in food]
-    food_prots = [f["prot_per_g"] for f in food]
-    food_carbs = [f["carb_per_g"] for f in food]
+    food_names = [f["name"] for f in foods]
+    food_cals = [f["cals_per_g"] for f in foods]
+    food_fats = [f["fat_per_g"] for f in foods]
+    food_carbs = [f["carb_per_g"] for f in foods]
+    food_prots = [f["prot_per_g"] for f in foods]
+    gram_multiplier = [f["serving_size"] for f in foods]
+    max_servings = [f["max_servings"] for f in foods]
+    max_grams = [f["max_servings"] * f["serving_size"] for f in foods]
 
     ####################################################################################################
+    # Problem definition
+    ####################################################################################################
 
-    target_kcals = 5 * 200
-    min_prot_pct = 0.4
-    max_fat_pct = 0.20
-    serving_size_g, max_servings = [None], [None]
-    max_servings = [
-        min(MAX_GRAMS // serving_size_g[i], max_servings[i])
-        for i in range(len(food_names))
-    ]
-
-    # Reset problem
+    # Create the 'prob' variable to contain the problem data
     prob = LpProblem("The food problem", LpMinimize)
 
+    # A dictionary called 'prob_vars' is created to contain the referenced Variables
     prob_vars = LpVariable.dicts(
         name="food",
         indices=food_names,
         lowBound=MIN_GRAMS,
-        upBound=max_servings,
+        upBound=max_grams,
         cat="Integer",
     )
 
     multipliers = LpVariable.dicts(
         "multiplier",
-        food_names,
-        lowBound=1,
-        upBound=[max_servings[food_names.index(f)] for f in food_names],
-        cat="Integer",
+        indices=food_names,
+        lowBound=0,
+        upBound=None,
+        cat="Continuous",
     )
 
     for f in food_names:
-        prob += (
-            prob_vars[f] == serving_size_g[food_names.index(f)] * multipliers[f],
-            f"Min grams of {f}",
-        )
+        var = prob_vars[f]
+        var.upBound = max_grams[food_names.index(f)]
 
     # Slack variables for constraints
+
     slack_protein = LpVariable("slack_protein", lowBound=0, cat="Continuous")
     slack_fat = LpVariable("slack_fat", lowBound=0, cat="Continuous")
     slack_kcals = LpVariable("slack_kcals", lowBound=0, cat="Continuous")
 
     # Objective = kcals
     prob += (
-        lpSum([prob_vars[f] * food_cals[f] for food in food_names])
+        lpSum([prob_vars[f] * food_cals[i] for i, f in enumerate(food_names)])
         + penalty_protein * slack_protein
         + penalty_fat * slack_fat
         + penalty_kcals * slack_kcals,
@@ -171,13 +110,6 @@ def solve_problem(params):
     # Solve
     prob.solve()
 
-    result = calculate_results(
-        prob=prob,
-        food_names=food_names,
-        food_prots=food_prots,
-        food_fats=food_fats,
-        food_carbs=food_carbs,
-        food_cals=food_cals,
-    )
+    result = parse_result(prob)
 
     return result
